4cc9f6ac2b42ba2f4ea3b0cff92fe3c6c887270d
cstuser <alihsz2002@gmail.com>
2020-01-31 14:02:57 -0500

    hello

---
+++ MyAgeIs.java
@@ -0,0 +1,14 @@
+public class MyAgeIs {
+	public static void main (String []args){
+
+		int myAge;
+		int currentYear = 2020;
+		int yearOfBirth = 2002;
+
+		myAge = currentYear - yearOfBirth;
+
+		System.out.println("My age is" + " " + myAge + "." );
+
+
+	}
+}
