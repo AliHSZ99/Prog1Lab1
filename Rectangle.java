0f046c644fd45c493df3e96ff96fec98ced45dc2
AliHSZ99 <alihsz2002@gmail.com>
2020-02-01 15:53:44 -0500

    Hello

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle{
+	public static void main(String[] args){
+
+		int width = 5;
+		int length = 10;
+		int area = width * length;
+		int perimeter = 2 * width + 2 * length;
+
+		System.out.println("The area of the rectangle is " + area + " and the perimeter is " + perimeter + ".");
+	}
+}
