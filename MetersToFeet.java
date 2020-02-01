262a85e990ee02b438308c18b7fc25bfd210c3f8
AliHSZ99 <alihsz2002@gmail.com>
2020-02-01 16:48:50 -0500

    Hello

---
+++ MetersToFeet.java
@@ -0,0 +1,9 @@
+public class MetersToFeet{
+	public static void main(String[] args){
+
+		float meters = 30f;
+		float metersToFeet = meters * 3.28f;   // 3.28 is the ratio from meters to feet
+
+		System.out.println(meters + " meters is " + metersToFeet + " feet.");
+	}
+}
