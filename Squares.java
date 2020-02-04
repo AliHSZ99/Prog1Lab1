f8576bd07bf745220866025a3e252e99c9d06ed8
AliHSZ99 <alihsz2002@gmail.com>
2020-02-03 19:54:16 -0500

    Hello

---
+++ Squares.java
@@ -0,0 +1,18 @@
+/* Doing this exercise where you have to calculate the square
+   of integers 1 through 9 with the things we have learned so far...
+   so there are no loops which would be much better.*/
+
+public class Squares{
+	public static void main(String [] args){
+
+		System.out.println("The square of 1 is " + 1*1);
+		System.out.println("The square of 2 is " + 2*2);
+		System.out.println("The square of 3 is " + 3*3);
+		System.out.println("The square of 4 is " + 4*4);
+		System.out.println("The square of 5 is " + 5*5);
+		System.out.println("The square of 6 is " + 6*6);
+		System.out.println("The square of 7 is " + 7*7);
+		System.out.println("The square of 8 is " + 8*8);
+		System.out.println("The square of 9 is " + 9*9);
+	}
+}
