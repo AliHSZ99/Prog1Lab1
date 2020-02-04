e16123247a96b7f17fb3c36acac868a8656307e5
AliHSZ99 <alihsz2002@gmail.com>
2020-02-03 21:27:55 -0500

    Hello

---
+++ CircleCircumference.java
@@ -0,0 +1,13 @@
+public class CircleCircumference{
+	public static void main(String [] args){
+
+		final double PI = 3.14159;
+		double radius = 3.2;
+
+		System.out.println("The radius of a circle is 3.2 inches.");
+		System.out.println("The circumference of this circle is " + 2*PI*radius + "inches \nusing the circumference formula which is C = 2πr.");
+		
+	}
+}
+
+      // Huge decimal problem!!!
