bc9d2f1901a62c8a58e6544a396458c1ccff2f4b
AliHSZ99 <alihsz2002@gmail.com>
2020-02-03 20:33:54 -0500

    Hello

---
+++ KgToLbs.java
@@ -0,0 +1,19 @@
+public class KgToLbs{
+	public static void main(String [] args){
+
+		
+		int kg1, kg2, kg3;
+		double ratio = 2.2;
+		kg1 = 10;
+		kg2 = 50;
+		kg3 = 100;
+
+		System.out.println("10kg is equal to " + kg1*ratio + "lbs."); // 2.2 is the ratio to convert
+		System.out.println("50kg is equal to " + kg2*ratio + "lbs."); // kg to lbs
+		System.out.println("100kg is equal to " + kg3*ratio + "lbs.");
+	}
+}
+
+
+   /* My result gives me a huge decimal, can you please tell me 
+   why this happens and also how I can fix it? */
