432b5c7bce4ddf6c8ae026e5ef3e2113d917b8a0
AliHSZ99 <alihsz2002@gmail.com>
2020-02-03 20:14:40 -0500

    Hello

---
+++ Average.java
@@ -0,0 +1,13 @@
+public class Average{
+	public static void main(String [] args){
+
+		double a, b, c, d;
+		a = 1;
+		b = 7;
+		c = 9;
+		d = 34;
+
+        System.out.println("The average of the integers 1, 7, 9, and 34 is " + (a + b + c +d)/4);
+
+	}
+}
